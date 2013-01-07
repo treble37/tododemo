fa2aad98c72f64919dde2fe8c4a5cb438a4b73c2
BruceP <brucepark0@gmail.com>
2013-01-07 14:30:01 -0800

    application.html.erb modified w/bootstrap html, modified tasks controller to find taskable, allow tasks#index to display link to new task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,16 @@
 class TasksController < ApplicationController
+	before_filter :find_taskable
+  
   def index
+  	@tasks = @taskable.tasks
   end
 
   def new
+  end
+
+private
+  def find_taskable
+  	klass = [Todouser, Folder].detect { |x| params["#{x.name.underscore}_id"]}
+  	@taskable = klass.find(params["#{klass.name.underscore}_id"])
   end
 end
